e63fa7f197a1f10a125bf2e471b36b53a1c7f182
Alyani Mamad <alyanimamadbhai@gmail.com>
2023-03-10 17:43:36 +0530

    this is my try

--- try.java
+++
@@ -1,5 +0,0 @@
-class try{
-    public static void mainn(String args[]){
-        System.out.println("Hello Buddy");
-    }
-}
---
+++ tryy.java
@@ -0,0 +1,11 @@
+class ok{
+    ok(){
+        System.out.println("This is ok");
+    }
+}
+public class tryy{
+    public static void main(String args[]){
+        ok obj=new ok();
+        System.out.println("Hello Buddy");
+    }
+}
