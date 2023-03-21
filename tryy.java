cba1ca85e83c13aeb31195851435ed605c7589d7
AlyaniMamad <97268754+AlyaniMamad@users.noreply.github.com>
2023-03-21 18:49:53 +0530

    Update tryy.java

--- tryy.java
+++ tryy.java
@@ -7,5 +7,7 @@ public class tryy{
     public static void main(String args[]){
         ok obj=new ok();
         System.out.println("Hello Buddy");
+         System.out.println();
+        
     }
 }
