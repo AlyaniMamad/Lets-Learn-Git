6ac0620ef047ffd28d51b1b0114743b33bc6a8fa
AlyaniMamad <97268754+AlyaniMamad@users.noreply.github.com>
2023-03-23 11:51:19 +0530

    Update anothertry.java

--- anothertry.java
+++ anothertry.java
@@ -2,6 +2,7 @@ public class anothertry {
     public static void main() {
         System.out.println("hello");
         System.out.println("HOW ARWE YOU?");
-        switch(ch)
+        switch(ch){
+                
     }
 }
