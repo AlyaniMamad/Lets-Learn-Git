84c07cf6d9d4234db7c82abc8895b561da5515a9
Alyani Mamad <97268754+AlyaniMamad@users.noreply.github.com>
2023-06-25 15:07:20 +0530

    Some Error Has been fixed
    Co-Authored-By: Kher_Akshay7 <64765656+akshay0077@users.noreply.github.com>

--- tryy.java
+++ tryy.java
@@ -8,7 +8,7 @@ public class tryy{
     public static void main(String args[]){
         ok obj=new ok();
         System.out.println();
-        System.out.println("Hello Buddy");
+        System.out.println("Chacha O Chacha");
          System.out.println();
         
     }
